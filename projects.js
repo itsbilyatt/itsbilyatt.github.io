c610fc1f98ce362544bd3a62a416d6f461795519
prajyot birajdar <106489353+itsbilyatt@users.noreply.github.com>
2025-05-04 13:35:42 +0530

    Update projects.js

--- projects.js
+++ projects.js
@@ -6,6 +6,23 @@ const projects = [
     skills:  ["Python", "Streamlit", "Pandas", "Seaborn", "Matplotlib", "NumPy", "Regular Expressions (re)"],
     demo: "https://dataexplorationandunivariatedataanalysis.streamlit.app/",
     github: "https://github.com/itsbilyatt/data_exploration_and_univariate_data_analysis"
+  },
+  {
+    title: "Security Analyst",
+    shortDesc: "Generative Ai chatbot & Dashboard to monitor mail security breaches",
+    fullDesc: "AutoDataAnalytics is an interactive tool designed for data exploration and univariate data analysis. It simplifies the process of analyzing and understanding datasets, providing users with key insights and visualizations through an intuitive interface built using Streamlit. Users can upload CSV or Excel files to explore dataset features, perform univariate analysis, and visualize data distributions. The tool provides essential features such as missing value detection, correlation matrices, summary statistics, and various visualization options including histograms, bar charts, and pie charts.",
+    skills:  ["Python", "Streamlit", "Pandas", "Seaborn", "Matplotlib", "NumPy", "Regular Expressions (re)" , "Gemini", "LLM", "Visualization", "data analysis"],
+    demo: "https://techwizardthaleshackethon.streamlit.app/",
+    github: "https://github.com/itsbilyatt/Thales-Hackathon-Techwizard"
+  },
+
+  {
+    title: "Air quality Index prediction",
+    shortDesc: "Machine learning solution to predict aqi using historical data",
+    fullDesc: "To do data analysis on India Air Quality data and predict tha value of Air Quality Index based on given features of concentration of sulphur dioxide,nitrogen dioxide, respirable suspended particualte matter, suspended particulate matter and classify the Air Quality as good, moderate, poor, unhealthy, healthy.",
+    skills:  ["Python", "Streamlit", "Pandas", "Seaborn", "Matplotlib", "NumPy", "Regular Expressions (re)" , "Machine learning", "data analysis"],
+    demo: "https://air-quality-index-predictor.streamlit.app/",
+    github: "https://github.com/itsbilyatt/Air-Quality-index-predictor/"
   }
   
 ];
